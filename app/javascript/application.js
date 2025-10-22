2329522fefd659ca887077cd7e41465123441a4a
Igor Alexandre <igoralexandrevieira652@gmail.com>
2025-10-22 13:21:55 -0400

    Importacoes do template mazer adiconadas. Package.json ajustado para compilar apenas js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,2 @@
-// Import Turbo (substitui rails-ujs)
-import "@hotwired/turbo-rails"
-
-// Import Stimulus
-import { Application } from "@hotwired/stimulus"
-
-// Inicializa Stimulus
-const application = Application.start()
-
-// Carrega todos os controllers automaticamente
-const context = require.context("./controllers", true, /\.js$/)
-context.keys().forEach((key) => {
-  const controller = context(key).default
-  if (controller) {
-    const name = key.replace("./", "").replace(".js", "")
-    application.register(name, controller)
-  }
-})
+import "./vendor/app"
+import "./vendor/initTheme"
